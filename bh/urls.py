ac44d1fa2ba71a0e39bc673f3acc6e74926557bb
Luciano Muñoz <luchisds@gmail.com>
2016-12-21 12:23:13 -0300

    changes in urls

--- bh/urls.py
+++ bh/urls.py
@@ -3,9 +3,13 @@ from django.contrib import admin
 
 from website import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
+
 	url(r'^admin/', admin.site.urls),
 	url(r'^$', views.index, name='home'),
 	url(r'^empresa/', views.company, name='company'),
 	url(r'^contacto/', views.contact, name='contact'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
