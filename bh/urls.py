6b814d4c1bd679ee3f9a785a762cc5b173bb5a1c
Luciano Muñoz <luchisds@gmail.com>
2016-12-21 12:25:49 -0300

    static files serve

--- bh/urls.py
+++ bh/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-
+	#static.serve debe ser usado solo en dev environment Fuck!
+    url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.STATIC_ROOT}),
 	url(r'^admin/', admin.site.urls),
 	url(r'^$', views.index, name='home'),
 	url(r'^empresa/', views.company, name='company'),
