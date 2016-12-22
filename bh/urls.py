6aa5dc20b43be437e041d3791163d322fb317c02
Luciano Muñoz <luchisds@gmail.com>
2016-12-22 08:43:33 -0300

    change gitignore & add ctacte

--- bh/urls.py
+++ bh/urls.py
@@ -8,9 +8,10 @@ from django.conf import settings
 
 urlpatterns = [
 	#static.serve debe ser usado solo en dev environment Fuck!
-    url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.STATIC_ROOT}),
+    #url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.STATIC_ROOT}),
 	url(r'^admin/', admin.site.urls),
 	url(r'^$', views.index, name='home'),
 	url(r'^empresa/', views.company, name='company'),
 	url(r'^contacto/', views.contact, name='contact'),
+	url(r'^ctacte/', views.ctacte, name='ctacte'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- website/views.py
+++ website/views.py
@@ -3,8 +3,18 @@ from django.shortcuts import render
 def index(request):
 	return render(request, 'index.html')
 
+
 def company(request):
 	return render(request, 'company.html')
 
+
 def contact(request):
 	return render(request, 'contact.html')
+
+
+def ctacte(request):
+	f = open('CtaCteP.TXT', 'r')
+	file = f.read()
+	print file
+
+	return render(request, 'ctacte.html', {'file':file})
