b22f0033874918ee1ee87578ac7a4a06f3f7da4c
Luciano Muñoz <luchisds@gmail.com>
2016-12-28 01:11:27 -0300

    user creation model

--- website/admin.py
+++ website/admin.py
@@ -3,15 +3,14 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 
 from website.forms import CustomUserCreationForm
-# website.forms import CustomUserChangeForm
+from website.forms import CustomUserChangeForm
 from website.models import User
 from website.models import CtaCte
 
 class UserAdmin(BaseUserAdmin):
-	#form = CustomUserChangeForm
+	form = CustomUserChangeForm
 	add_form = CustomUserCreationForm
 
-	list_display = ('user', 'password', 'algoritmo_code',)
 
 # Register models
 admin.site.register(User, UserAdmin)
