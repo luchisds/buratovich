2a5a831f8b808d6d71df8645e2cd90963e66cc81
Luciano Muñoz <luchisds@gmail.com>
2017-01-20 17:57:45 -0300

    change database from sqlite3 to postgres

--- bh/settings.py
+++ bh/settings.py
@@ -67,10 +67,21 @@ WSGI_APPLICATION = 'bh.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.sqlite3',
+# 		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# 	}
+# }
+
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'buratovich',
+		'USER': 'postgres',
+		'PASSWORD': 'luciano',
+		'HOST': 'localhost',
+		'PORT': '5432',
 	}
 }
 
