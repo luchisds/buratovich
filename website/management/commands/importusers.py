7c31576f719101f6477da8329423987469fe954f
Luciano Muñoz <luchisds@gmail.com>
2017-02-24 12:30:39 -0300

    develop importusers command

--- website/management/commands/importusers.py
+++ website/management/commands/importusers.py
@@ -1,30 +1,40 @@
 import csv
+import os
+
+from django.conf import settings
 from django.contrib.auth.models import User
-from auth.models import UserProfile
-from website.models import UserInfo
+from django.core.management.base import BaseCommand, CommandError
 from django.db import IntegrityError
 
-from django.core.management.base import BaseCommand, CommandError
+from website.models import UserInfo
 
 class Command(BaseCommand):
 	help = 'Import users from csv file'
 
 	def handle(self, *args, **options):
-		members = open('eclub_data.csv', "rU")
-		data = csv.DictReader(members)
-		default_password = 'tsc_eclub'
+		file = os.path.join(settings.BASE_DIR, 'cuentas.csv')
+		with open(file, 'r') as accounts:
 
-		for row in data:
-			email = row['Email']
-			if email_re.match(email):
-				tokens = email.split('@')
-				username = tokens[0]  
+			# default_password = 'tsc_eclub'
+
+			reader = csv.reader(accounts, delimiter=';')
+			# First line is header
+			reader.next()
+
+			for row in reader:
+				cod = row[0]
+				name = row[1]
+				user = row[2]
+				passw = row[3]
+				email = ''
 				try:
-					user = User.objects.create_user(username, email, default_password)
+					user = User.objects.create_user(user, email, passw)
 					user.is_staff = False
 					user.save()
-					profile = UserProfile(user_id=user.id)
-					profile.save()
-					self.stdout.write(self.style.SUCCESS('Successfully closed poll'))
+					userinfo = UserInfo(user_id=user.id)
+					userinfo.algoritmo_code = cod
+					userinfo.company_name = name
+					userinfo.save()
+					self.stdout.write(self.style.SUCCESS('Successfully created user "%s"' % name))
 				except IntegrityError:
-					print username
+					print name
