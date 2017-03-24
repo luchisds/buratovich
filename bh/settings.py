8dd2972aeaada95ed2c92db616abdd8968cdadfe
Luciano Muñoz <luchisds@gmail.com>
2017-03-24 19:07:43 -0300

    cp online view

--- bh/settings.py
+++ bh/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
