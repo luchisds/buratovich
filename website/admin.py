b3e69e5a6320304c2998bfaca5bcfeec7fd4bcd2
Luciano Muñoz <luchisds@gmail.com>
2017-03-29 13:39:26 -0300

    admin customization

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
@@ -38,11 +40,16 @@ class UserInline(admin.StackedInline):
 	verbose_name_plural = 'Informacion Algoritmo'
 	exclude = ('account_confirmed', 'random_password',)
 
+	inline_classes = ('grp-collapse grp-open',)
+
 
 class UserAdmin(BaseUserAdmin):
 	inlines = (UserInline, )
 	add_form = UserCreateForm
-	
+
+	list_display = ('username', 'email', 'get_company_name', 'get_algoritmo_code', 'is_active', 'is_staff',)
+	empty_value_display = ''
+
 	add_fieldsets = (
 		(None, {
 			'classes': ('wide',),
@@ -50,8 +57,23 @@ class UserAdmin(BaseUserAdmin):
 		),
 	)
 
+	def get_company_name(self, obj):
+		return obj.userinfo.company_name
+
+	def get_algoritmo_code(self, obj):
+		return obj.userinfo.algoritmo_code
+
+	get_company_name.short_description = 'Razón Social'
+	get_algoritmo_code.short_description = 'Código Algoritmo'
+	get_company_name.admin_order_field = 'userinfo__company_name'
+	get_algoritmo_code.admin_order_field = 'userinfo__algoritmo_code'
+
 
 class CurrenciesAdmin(admin.ModelAdmin):
+	list_display = ('date', 'get_dn_buy', 'get_dn_sell', 'get_dl_buy', 'get_dl_sell',)
+	empty_value_display = ''
+	date_hierarchy = 'date'
+
 	fieldsets = (
 		(None, {
 			'fields': ('date',),
@@ -64,8 +86,33 @@ class CurrenciesAdmin(admin.ModelAdmin):
 		}),
 	)
 
+	def get_dn_buy(self, obj):
+		return obj.dn_buy
+
+	def get_dn_sell(self, obj):
+		return obj.dn_sell
+
+	def get_dl_buy(self, obj):
+		return obj.dl_buy
+
+	def get_dl_sell(self, obj):
+		return obj.dl_sell
+
+	get_dn_buy.short_description = 'Nación Compra'
+	get_dn_sell.short_description = 'Nación Venta'
+	get_dl_buy.short_description = 'Libre Compra'
+	get_dl_sell.short_description = 'Libre Venta'
+	get_dn_buy.admin_order_field = 'dn_buy'
+	get_dn_sell.admin_order_field = 'dn_sell'
+	get_dl_buy.admin_order_field = 'dl_buy'
+	get_dl_sell.admin_order_field = 'dl_sell'
+
 
 class BoardAdmin(admin.ModelAdmin):
+	list_display = ('date',)
+	empty_value_display = ''
+	date_hierarchy = 'date'
+
 	fieldsets = (
 		(None, {
 			'fields': ('date',),
@@ -92,17 +139,39 @@ class RainDetailInline(admin.StackedInline):
 	verbose_name = 'Detalle de Lluvia'
 	verbose_name_plural = 'Detalle de Lluvias'
 
+	inline_classes = ('grp-collapse grp-open',)
+
 
 class RainAdmin(admin.ModelAdmin):
 	inlines = (RainDetailInline, )
+	date_hierarchy = 'date'
+
+
+class CityAdmin(admin.ModelAdmin):
+	list_display = ('city', 'state',)
+	empty_value_display = ''
+
+
+class NotificationsAdmin(admin.ModelAdmin):
+	list_display = ('date', 'title', 'date_from', 'date_to', 'active', 'notification')
+
+
+class ViewedNotificationsAdmin(admin.ModelAdmin):
+	list_display = ('get_company_name', 'notification',)
+
+	def get_company_name(self, obj):
+		return obj.user.userinfo.company_name
+
+	get_company_name.short_description = 'Razón Social'
+	get_company_name.admin_order_field = 'user__userinfo__company_name'
 
 
 # Register models
 admin.site.register(User, UserAdmin)
 #admin.site.register(CtaCte)
-admin.site.register(Notifications)
-admin.site.register(ViewedNotifications)
+admin.site.register(Notifications, NotificationsAdmin)
+admin.site.register(ViewedNotifications, ViewedNotificationsAdmin)
 admin.site.register(Currencies, CurrenciesAdmin)
 admin.site.register(Board, BoardAdmin)
-admin.site.register(City)
+admin.site.register(City, CityAdmin)
 admin.site.register(Rain, RainAdmin)
