962598b6d33e12221729a12cb3f5c0369b397636
Luciano Muñoz <luchisds@gmail.com>
2017-04-02 12:29:42 -0300

    separate import users and send account activation email

--- website/management/commands/importusers.py
+++ website/management/commands/importusers.py
@@ -11,9 +11,10 @@ from django.db import IntegrityError
 from django.db.models.signals import post_save
 from django.db.models.signals import pre_save
 from website.signals import postSave_User, preSave_User
+from website.tokens import account_activation_token
+from django.utils.http import urlsafe_base64_encode
 
 from website.models import UserInfo
-
 
 
 class Command(BaseCommand):
@@ -22,12 +23,13 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 
 		# Disconnect signals
-		pre_save.disconnect(receiver= preSave_User, sender=User)
-		post_save.disconnect(receiver= postSave_User, sender=User)
+		pre_save.disconnect(receiver= preSave_User, sender=User, dispatch_uid='website.signals.preSave_User')
+		post_save.disconnect(receiver= postSave_User, sender=User, dispatch_uid='website.signals.postSave_User')
 
 		txt = os.path.join(settings.BASE_DIR, 'cuentas.txt')
 
 		new_accounts = open(os.path.join(settings.BASE_DIR, 'new_accounts.txt'), 'a')
+		new_accounts.write('Codigo' + '\t' + 'Nombre' + '\t'+ 'Usuario' + '\t' + 'Email' + '\t' + 'Password' + '\t' + 'Token' + '\t' + 'UID' + '\n')
 
 		with open(txt, 'r') as accounts:
 
@@ -47,8 +49,9 @@ class Command(BaseCommand):
 					email = re.split(';', email)
 					email = email[0]
 					try:
+						# User
 						passw = User.objects.make_random_password(8)
-						print passw
+						print cod, name, user, email, passw
 						user = User.objects.create_user(user, email, passw)
 						print "user created"
 						user.is_staff = False
@@ -56,6 +59,7 @@ class Command(BaseCommand):
 						user.save()
 						print "user saved"
 
+						# UserInfo
 						userinfo = UserInfo(user_id=user.id)
 						userinfo.algoritmo_code = cod
 						userinfo.company_name = name
@@ -64,11 +68,15 @@ class Command(BaseCommand):
 						userinfo.save()
 						print "userinfo created"
 
-						new_line = cod + '\t' + name + '\t' + user + '\t' + email + '\t' + passw + '\n'
+						# Token
+						token = account_activation_token.make_token(user)
+						uidb64 = urlsafe_base64_encode(str(user.pk))
+
+						new_line = cod + '\t' + name + '\t' + user.username + '\t' + email + '\t' + passw + '\t' + token + '\t' + uidb64 + '\n'
 						new_accounts.write(new_line)
 
 						self.stdout.write(self.style.SUCCESS('Successfully created user "%s"' % name))
 					except IntegrityError:
-						print name
+						print 'Error', name
 
 		new_accounts.close()
