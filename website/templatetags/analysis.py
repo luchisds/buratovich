42d4c1ac07d944466a8f91624fa8892793ecc209
Luciano Muñoz <luchisds@gmail.com>
2017-04-27 12:32:08 -0300

    deliveries and sales responsive

--- website/templatetags/analysis.py
+++ website/templatetags/analysis.py
@@ -5,5 +5,5 @@ register = template.Library()
 @register.assignment_tag
 def get_analysis(ticket_analysis, voucher):
 	detail = ticket_analysis.get(voucher)
-	print voucher, detail
+	#print voucher, detail
 	return detail
