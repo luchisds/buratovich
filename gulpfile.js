a4b3045d496519a83e85972a27b902932c8064b5
Luciano Muñoz <luchisds@gmail.com>
2017-05-04 12:26:17 -0300

    add new gulp automation functions

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,22 @@ var stylus = require('gulp-stylus');
 var jeet = require('jeet');
 var autoprefixer = require('autoprefixer-stylus');
 var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
 var spawn = require('child_process').spawn;
 
+var argv = require('yargs').argv;
+
+// Routes
+var build = 'website/build';
+
+
+// gulp build --production
+var production = !!argv.production;
+
+
+// Start DJANGO server
 gulp.task('serve:backend', function () {
 	var devServerPort = process.env.PORT || 8000;
 	process.env.PYTHONUNBUFFERED = 1;
@@ -14,22 +28,47 @@ gulp.task('serve:backend', function () {
 	});
 });
 
+// Minify JS
 gulp.task('js', function(){
 	gulp.src('website/static/js/**/*.js')
 		.pipe(uglify())
-		.pipe(gulp.dest('./build'));
+		.pipe(gulp.dest(build + '/js'));
 });
 
+// Process Stylus and compress CSS
 gulp.task('styles', function() {
 	gulp.src('website/static/css/styles.styl')
 		.pipe(stylus({
+			compress: true,
 			use: [jeet(), autoprefixer({browsers:['last 3 versions']})]
 		}))
-		.pipe(gulp.dest('./build'));
+		.pipe(gulp.dest(build + '/css'));
+});
+
+// Minify HTML
+gulp.task('html', function() {
+	gulp.src('website/templates/*.html')
+		.pipe(htmlmin({
+			collapseWhitespace: true, 
+			empty: true
+		}))
+		.pipe(gulp.dest(build + '/templates'));
+});
+
+// Optimize Images
+gulp.task('img', function() {
+	gulp.src('website/static/img/**/*.*')
+		.pipe(changed('./build/img'))
+		.pipe(imagemin({
+			verbose: true
+		}))
+		.pipe(gulp.dest(build + '/img'));
 });
 
 gulp.task('default', function() {
 	gulp.start('serve:backend');
 	gulp.watch('website/static/css/*.styl', ['styles']);
 	gulp.watch('website/static/js/**/*.js', ['js']);
+	gulp.watch('website/templates/*.html', ['html']);
+	gulp.watch('website/static/img/**/*.*', ['img']);
 });
