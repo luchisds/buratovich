1413a95647272ecd99fcbea29aeda951639942a4
Luciano Muñoz <luchisds@gmail.com>
2017-06-23 17:18:19 -0300

    add is_commercial field & change views/templates to support commercial users

--- website/admin.py
+++ website/admin.py
@@ -38,7 +38,7 @@ class UserInline(admin.StackedInline):
 	can_delete = False
 	verbose_name = 'Informacion Algoritmo'
 	verbose_name_plural = 'Informacion Algoritmo'
-	exclude = ('account_confirmed', 'random_password',)
+	exclude = ('account_confirmed', 'random_password', 'is_commercial')
 
 	inline_classes = ('grp-collapse grp-open',)
 
@@ -47,7 +47,7 @@ class UserAdmin(BaseUserAdmin):
 	inlines = (UserInline, )
 	add_form = UserCreateForm
 
-	list_display = ('username', 'email', 'get_company_name', 'get_algoritmo_code', 'is_active', 'is_staff',)
+	list_display = ('username', 'email', 'get_company_name', 'get_algoritmo_code', 'get_is_commercial', 'is_active', 'is_staff',)
 	empty_value_display = ''
 
 	add_fieldsets = (
@@ -57,14 +57,20 @@ class UserAdmin(BaseUserAdmin):
 		),
 	)
 
+	def get_is_commercial(self, obj):
+		return obj.userinfo.is_commercial
+
 	def get_company_name(self, obj):
 		return obj.userinfo.company_name
 
 	def get_algoritmo_code(self, obj):
 		return obj.userinfo.algoritmo_code
 
+	get_is_commercial.boolean = True
+	get_is_commercial.short_description = 'Es Comercial'
 	get_company_name.short_description = 'Razón Social'
 	get_algoritmo_code.short_description = 'Código Algoritmo'
+	get_is_commercial.admin_order_field = 'userinfo__is_commercial'
 	get_company_name.admin_order_field = 'userinfo__company_name'
 	get_algoritmo_code.admin_order_field = 'userinfo__algoritmo_code'
 
