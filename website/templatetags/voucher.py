618d84587b42ae705b5eeaa4d5dc938a9d83fca1
Luciano Muñoz <luchisds@gmail.com>
2017-07-05 17:34:44 -0300

    fix voucher cod-nro when suc does not exist

--- website/templatetags/voucher.py
+++ website/templatetags/voucher.py
@@ -4,12 +4,20 @@ register = template.Library()
 
 @register.assignment_tag
 def voucher(voucher, with_cod):
+
+	def getVoucherNumber(vlist, n):
+		if n > 2:
+			# Suc / Num
+			return str(int(voucher[1])) + '-' + str(int(voucher[2]))
+		else:
+			# Num
+			return str(int(voucher[1]))
+
 	voucher = voucher.split(' ')
 	cod = voucher[0]
-	suc = str(int(voucher[1]))
-	nro = str(int(voucher[2]))
+	nro = getVoucherNumber(voucher, len(voucher))
 
 	if with_cod == 'S':
-		return cod + ' ' + suc + '-' + nro
+		return cod + ' ' + nro
 	elif with_cod == 'N':
-		return suc + '-' + nro
+		return nro
